69c8bf323e0b20b853ce60ba348cf6cbab3b4144
TimLedger <tim_ledger@mail.ru>
2024-03-14 14:33:25 +0600

    создан API, который шифрует и дешифрует текст на основании шифра Виженера

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = 'tledger';
 
 app.get('/', (req, res) => {
     res.send('<h1>Home page</h1>');
 });
 
-app.get("/:name", (req, res) => {
+app.get('/echo/:name', (req, res) => {
     res.send('<h1>' + req.params.name + '</h1>');
+});
+
+app.get('/encode/:string', (req, res) => {
+    res.send('<h1>' + Vigenere.Cipher(password).crypt(req.params.string) + '</h1>');
+});
+
+app.get('/decode/:string', (req, res) => {
+    res.send('<h1>' + Vigenere.Decipher(password).crypt(req.params.string) + '</h1>');
 });
 
 app.listen(port, () => {
